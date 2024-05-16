df5dd18f4a08f9c99bae8b98c6b86554a2b9ebfa
ᴛʜᴇ ᴄʜᴧᴍᴘᴜ <112340896+ChampuXD@users.noreply.github.com>
2024-05-16 10:28:23 +0530

    Update config.py

--- config.py
+++ config.py
@@ -32,7 +32,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/ChampuXD/ChampuXMusic",
+    "https://github.com/ChampuXD/TelegramMusicBot",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 GIT_TOKEN = getenv(
